697851cd0507c1002d6930ac2275ad1ccff0d277
iqbalululazmi <iqbalululazmi@gmail.com>
2021-05-03 23:15:54 +0700

    feat: try jest

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,28 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import TestRenderer from "react-test-renderer";
+import { MockedProvider } from "@apollo/client/testing";
+import Pokemon from "./pages/Pokemon/Pokemon";
+import Pokedex from "./pages/Pokedex/Pokedex";
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+const mocks: any[] = []; // We'll fill this in next
+
+it("renders pokemon component", () => {
+    const component = TestRenderer.create(
+        <MockedProvider mocks={mocks} addTypename={false}>
+            <Pokemon />
+        </MockedProvider>
+    );
+
+    const tree: any = component.toJSON();
+    expect(tree.children[0].children[0].type).toContain("svg");
+});
+
+it("renders pokedex component", () => {
+    const component = TestRenderer.create(
+        <MockedProvider mocks={mocks} addTypename={false}>
+            <Pokedex />
+        </MockedProvider>
+    );
+
+    const tree: any = component.toJSON();
+    expect(tree.children[0].children[0].type).toContain("img");
 });
